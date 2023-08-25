749e78de744f2aedb5bec148677d42fabbbb7aeb
Jeet Adhikari <jeet@backbase.com>
2023-08-25 18:31:39 +0200

    added env files

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { Login, Signup, User } from "../../../store/auth/types";
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -11,11 +12,11 @@ export class AuthService {
     constructor(private http: HttpClient) {}
 
     signup(user: Signup): Observable<User> {
-        return this.http.post<User>('/user', user)
+        return this.http.post<User>(`${environment.apiUrl}/user`, user)
     }
 
     login(user: Login): Observable<User> {
-        return this.http.post<User>('/login', user)
+        return this.http.post<User>(`${environment.apiUrl}/login`, user)
     } 
 
     logout(): Observable<boolean> {
