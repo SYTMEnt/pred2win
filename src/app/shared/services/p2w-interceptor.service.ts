416a6b76c62066b8726b1c9ec7e40f43cb065df7
Jeet Adhikari <jeet@backbase.com>
2023-08-26 23:39:34 +0200

    fixed responsiveness for tournaments

--- src/app/shared/services/p2w-interceptor.service.ts
+++ src/app/shared/services/p2w-interceptor.service.ts
@@ -1,22 +1,17 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable,  } from "rxjs";
-import { map, take } from 'rxjs/operators'
-import { AuthStoreService } from "src/app/store/auth/auth-store.service";
 import  { environment } from '../../../environments/environment'
 
 @Injectable()
 export class P2WInterceptor implements HttpInterceptor {
 
-    constructor(private authStoreService: AuthStoreService) {}
-
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        
         req = req.clone({
             url: `${environment.apiUrl}${req.url}`,
             withCredentials: true
         })
-
         return next.handle(req)
     }
+    
 }
