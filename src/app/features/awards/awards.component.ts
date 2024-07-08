44e57c649d9f139a91bfc496cf39ed2f9693a7ed
avik3992 <avik.chaudhuri87@gmail.com>
2024-07-08 20:10:28 +0200

    newTour

--- src/app/features/awards/awards.component.ts
+++ src/app/features/awards/awards.component.ts
@@ -2,50 +2,59 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AwardsService } from './services/awards.service';
 
-
 @Component({
   selector: 'app-awards',
   templateUrl: './awards.component.html',
   styleUrls: ['./awards.component.scss']
 })
 export class AwardsComponent implements OnInit {
-    filters = ['awards', 'streaks'];
-    selectedCategory: string = 'awards';
-    tournamentId = 'focomeeuro24';
-    pollType='tournament';
-    awardDetails: any;
-    loading = false;
-    error: string | undefined;
+  filters = ['awards', 'streaks'];
+  selectedCategory: string = 'awards';
+  pollType = 'tournament';
+  tournamentId: any;
+  awardDetails: any;
+  loading = false;
+  error: string | undefined;
 
-    constructor(private awardsService: AwardsService,private router: Router) {}
+  constructor(
+    private awardsService: AwardsService,
+    private router: Router,
+    private route: ActivatedRoute  
+  ) {}
 
-    ngOnInit(): void {
+  ngOnInit(): void {
+    // Get the tournamentId from the route parameters
+    this.route.paramMap.subscribe(params => {
+      this.tournamentId = params.get('tournamentId');
       this.fetchAwards();
-    }
+    });
+  }
 
-    onFilterSelect(category: string) {
-      this.selectedCategory = category;
-      this.fetchAwards();
-    }
+  onFilterSelect(category: string) {
+    this.selectedCategory = category;
+    this.fetchAwards();
+  }
 
   fetchAwards() {
-
     this.loading = true;
     this.error = undefined;
 
-    if (this.selectedCategory !== undefined) {
+    if (this.selectedCategory !== undefined && this.tournamentId) {
       this.awardsService.awards(this.tournamentId, this.selectedCategory)
-        .subscribe((awards) => {
-          this.awardDetails = awards;
-        },
-        (error) => {
-          this.error = error;
-        },
-        () => {
-          this.loading = false;
-        });
-    } 
-
+        .subscribe(
+          (awards) => {
+            this.awardDetails = awards;
+          },
+          (error) => {
+            this.error = error;
+          },
+          () => {
+            this.loading = false;
+          }
+        );
+    } else {
+      this.loading = false;
+      this.error = 'Tournament ID is missing or invalid';
+    }
   }
-
 }
